4dfa897af17693e74e02a890f09aa24e0a4286cd
Ben Rehmann <brehmann@me.com>
2024-02-13 16:12:24 -0600

    login form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 
 function LoginForm() {
   const [username, setUsername] = useState('');
@@ -25,42 +25,93 @@ function LoginForm() {
   }; // end login
 
   return (
-    <form className="formPanel" onSubmit={login}>
-      <h2>Login</h2>
-      {errors.loginMessage && (
-        <h3 className="alert" role="alert">
-          {errors.loginMessage}
-        </h3>
-      )}
-      <div>
-        <label htmlFor="username">
-          Username:
-          <input
-            type="text"
-            name="username"
-            required
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <label htmlFor="password">
-          Password:
-          <input
-            type="password"
-            name="password"
-            required
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <input className="btn" type="submit" name="submit" value="Log In" />
-      </div>
-    </form>
+
+    <div className='clapboard'>
+
+      <form onSubmit={login}>
+
+        <div className='clapTop'>
+          <div className='clapStrip'>
+            <div className='chevronUp yellow'></div>
+            <div className='chevronUp red'></div>
+            <div className='chevronUp blue'></div>
+            <div className='chevronUp'></div>
+            <div className='chevronUp white'></div>
+            <div className='chevronUp'></div>
+          </div>
+          <div className='clapStrip'>
+            <div className='chevronDw yellow'></div>
+            <div className='chevronDw red'></div>
+            <div className='chevronDw blue'></div>
+            <div className='chevronDw'></div>
+            <div className='chevronDw white'></div>
+            <div className='chevronDw'></div>
+          </div>
+        </div>
+
+
+        <div className='clapBody'>
+          <div className='clapRowOne'>
+            <div className='clapBoxOne'>
+              <h2>Login</h2>
+            </div>
+            <div className='clapBoxOne boxMid'></div>
+            <div className='clapBoxOne'></div>
+          </div>
+
+          <div className='clapRowTwo'>
+            <div className='clapLog'>
+              <div className='clapBoxLog boxTop'>
+                <label htmlFor="username">
+                  E-mail:
+                  <br />
+                  <input
+                    type="text"
+                    name="username"
+                    className='txtBox'
+                    required
+                    value={username}
+                    onChange={(event) => setUsername(event.target.value)}
+                  />
+                </label>
+              </div>
+              <div className='clapBoxLog'>
+                <label htmlFor="password">
+                  Password:
+                  <br />
+                  <input
+                    type="password"
+                    name="password"
+                    required
+                    className='txtBox'
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                  />
+                </label>
+              </div>
+            </div>
+            <div className='clapBoxTwo'>
+              <input className="btn" type="submit" name="submit" value="Log In" />
+            </div>
+          </div>
+
+          <div className='clapRowThree'>
+            <p>This is a little bit about The 6th Degree!</p>
+          </div>
+        </div>
+
+        {errors.loginMessage && (
+          <h3 className="alert" role="alert">
+            {errors.loginMessage}
+          </h3>
+        )}
+
+      </form>
+
+    </div>
+
   );
 }
 
 export default LoginForm;
+
