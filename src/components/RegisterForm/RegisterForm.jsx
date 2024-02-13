f2a5289c687ba96c4fe27ef8f15dd593dd821bae
Ben Rehmann <brehmann@me.com>
2024-02-13 16:13:05 -0600

    register form

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -20,46 +20,95 @@ function RegisterForm() {
   }; // end registerUser
 
   return (
-    <form onSubmit={registerUser}>
-      <h2>Register User</h2>
-      {errors.registrationMessage && (
-        <h3 className="alert" role="alert">
-          {errors.registrationMessage}
-        </h3>
-      )}
-      <div>
-        <label htmlFor="username">
-          Username:
-          <input
-            type="text"
-            name="username"
-            value={username}
-            required
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <label htmlFor="password">
-          Password:
-          <input
-            type="password"
-            name="password"
-            value={password}
-            required
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </label>
-      </div>
+
+    <div className='clapboard'>
+
+      <form onSubmit={registerUser}>
+
+      <div className='clapTop'>
+          <div className='clapStrip'>
+            <div className='chevronUp'></div>
+            <div className='chevronUp'></div>
+            <div className='chevronUp'></div>
+            <div className='chevronUp'></div>
+            <div className='chevronUp'></div>
+            <div className='chevronUp'></div>
+          </div>
+          <div className='clapStrip'>
+            <div className='chevronDw'></div>
+            <div className='chevronDw'></div>
+            <div className='chevronDw'></div>
+            <div className='chevronDw'></div>
+            <div className='chevronDw'></div>
+            <div className='chevronDw'></div>
+          </div>
+        </div>
 
 
-      <div>
-        <input className="btn" type="submit" name="submit" value="Register" />
-      </div>
+        <div className='clapBody'>
+          <div className='clapRowOne'>
+            <div className='clapBoxOne'>
+              <h2>Register User</h2>
+            </div>
+            <div className='clapBoxOne boxMid'></div>
+            <div className='clapBoxOne'></div>
+          </div>
 
-      
-    </form>
+
+          <div className='clapRowTwo'>
+            <div className='clapLog'>
+              <div className='clapBoxLog boxTop'>
+                <label htmlFor="username">
+                  E-mail:
+                  <br />
+                  <input
+                    type="text"
+                    name="username"
+                    className='txtBox'
+                    value={username}
+                    required
+                    onChange={(event) => setUsername(event.target.value)}
+                  />
+                </label>
+              </div>
+              <div className='clapBoxLog'>
+                <label htmlFor="password">
+                  Password:
+                  <br />
+                  <input
+                    type="password"
+                    name="password"
+                    className='txtBox'
+                    value={password}
+                    required
+                    onChange={(event) => setPassword(event.target.value)}
+                  />
+                </label>
+              </div>
+            </div>
+            <div className='clapBoxTwo'>
+              <input className="btn" type="submit" name="submit" value="Register" />
+            </div>
+          </div>
+
+          <div className='clapRowThree'>
+            <p>This is a little bit about The 6th Degree!</p>
+          </div>
+        </div>
+
+        {errors.registrationMessage && (
+          <h3 className="alert" role="alert">
+            {errors.registrationMessage}
+          </h3>
+        )}
+
+      </form>
+
+    </div>
+
   );
 }
 
 export default RegisterForm;
+
+
