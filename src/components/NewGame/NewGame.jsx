d061d414985321318af49371119dab2feec99926
Noah Greensweig <ngreensweig@gmail.com>
2024-02-16 12:57:40 -0600

    Add createGame saga to get POST route working for 'Create Game' button on new-game route.

--- src/components/NewGame/NewGame.jsx
+++ src/components/NewGame/NewGame.jsx
@@ -1,52 +1,63 @@
-
+import { useDispatch } from "react-redux";
 function NewGame() {
-     return (
-        <body>
-            <h1>Pick 3 Scripts!</h1>
-            <button classname="create-game-btn"> Create Game </button> 
 
-        <li class="container">Jennifer Aniston to Ben Stiller
-  <input type="checkbox"></input>
-  <span class="checkmark"></span>
-</li>
+  const dispatch = useDispatch();
 
-<li class="container">Dwayne Johnson to Jennifer Lopez
-  <input type="checkbox"></input>
-  <span class="checkmark"></span>
-</li>
 
-<li class="container">Ryan Reynolds to Queen Latifah
-  <input type="checkbox"></input>
-  <span class="checkmark"></span>
-</li>
+  const handleClick = (e) => {
+    e.preventDefault();
+    
+    // Create new game in DB
+    dispatch({ type: 'CREATE_GAME' });
+  };
 
-<li class="container">Shaq to Brad Pitt
-  <input type="checkbox"></input>
-  <span class="checkmark"></span>
-</li>
-<li class="container">George Clooney to Robert Downey Jr
-  <input type="checkbox"></input>
-  <span class="checkmark"></span>
-</li>
+  return (
+    <body>
+      <h1>Pick 3 Scripts!</h1>
+      <button classname="create-game-btn" onClick={e => handleClick(e)}> Create Game </button>
 
-<li class="container">Tyler Perry to Merryll Streep
-  <input type="checkbox"></input>
-  <span class="checkmark"></span>
-</li>
+      <li class="container">Jennifer Aniston to Ben Stiller
+        <input type="checkbox"></input>
+        <span class="checkmark"></span>
+      </li>
 
-<li class="container">Ryan Gosling to Matthew McConaughey
-  <input type="checkbox"></input>
-  <span class="checkmark"></span>
-</li>
+      <li class="container">Dwayne Johnson to Jennifer Lopez
+        <input type="checkbox"></input>
+        <span class="checkmark"></span>
+      </li>
 
-<li class="container">Margot Robbie to Arnold Schwarzenegger
-  <input type="checkbox"></input>
-  <span class="checkmark"></span>
-</li>
+      <li class="container">Ryan Reynolds to Queen Latifah
+        <input type="checkbox"></input>
+        <span class="checkmark"></span>
+      </li>
 
-<button classname="create-script-btn"> Create New Script </button> 
-        </body>
-     )
+      <li class="container">Shaq to Brad Pitt
+        <input type="checkbox"></input>
+        <span class="checkmark"></span>
+      </li>
+      <li class="container">George Clooney to Robert Downey Jr
+        <input type="checkbox"></input>
+        <span class="checkmark"></span>
+      </li>
+
+      <li class="container">Tyler Perry to Merryll Streep
+        <input type="checkbox"></input>
+        <span class="checkmark"></span>
+      </li>
+
+      <li class="container">Ryan Gosling to Matthew McConaughey
+        <input type="checkbox"></input>
+        <span class="checkmark"></span>
+      </li>
+
+      <li class="container">Margot Robbie to Arnold Schwarzenegger
+        <input type="checkbox"></input>
+        <span class="checkmark"></span>
+      </li>
+
+      <button classname="create-script-btn"> Create New Script </button>
+    </body>
+  )
 }
 
 
--- src/redux/sagas/game.saga.js
+++ src/redux/sagas/game.saga.js
@@ -2,18 +2,25 @@ import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
 
 function* fetchGame() {
-
     try {
         const response = yield axios.get('/api/game');
         yield put({ type: 'SET_GAME', payload: response.data });
     } catch (error) {
         console.log('Game GET request failed', error);
     }
+}
 
+function* createGame() {
+    try {
+        yield axios.post('/api/game');
+    } catch (error) {
+        console.log('Game POST request failed', error);
+    }
 }
 
 function* gameSaga() {
     yield takeLatest('FETCH_GAME', fetchGame);
+    yield takeLatest('CREATE_GAME', createGame);
 }
 
 export default gameSaga;
