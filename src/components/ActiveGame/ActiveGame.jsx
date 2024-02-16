0d26a89bec7e0b5154f3589a5ac7e6c7d47a23d4
Stephon Tonge <stephon.tonge75@gmail.com>
2024-02-15 18:39:34 -0600

    active game changes

--- src/components/ActiveGame/ActiveGame.jsx
+++ src/components/ActiveGame/ActiveGame.jsx
@@ -1,206 +1,104 @@
 import { Box, FormControl, TextField, Button } from "@mui/material";
-
 import React from "react";
 import "./ActiveGame.css";
 
 function ActiveGame() {
   return (
-    <Box sx={{ display: "flex", flexWrap: "wrap" }}>
+    <Box
+      component="form"
+      noValidate
+      sx={{
+        mt: 1,
+        alignItems: "center",
+        marginTop: 8,
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
       <h2>Active game</h2>
       <br />
       <FormControl>
         <TextField
           label="Who:"
           id="outlined-start-adornment"
-          sx={{ m: 1, width: "25ch" }}
-          InputProps={{
-            sx: {
-              color: "black",
-              backgroundColor: "white",
-              "&.Mui-focused": { color: "black", backgroundColor: "white" },
-            },
-          }}
-          variant="filled"
+          color="secondary"
           required
           fullWidth
         />
         <TextField
           label="Is in:"
           id="outlined-start-adornment"
-          sx={{ m: 1, width: "25ch" }}
-          InputProps={{
-            sx: {
-              color: "black",
-              backgroundColor: "white",
-              "&.Mui-focused": { color: "black", backgroundColor: "white" },
-            },
-          }}
-          variant="filled"
+          color="secondary"
           required
           fullWidth
         />
         <TextField
           label="With:"
           id="outlined-start-adornment"
-          sx={{ m: 1, width: "25ch" }}
-          InputProps={{
-            sx: {
-              color: "black",
-              backgroundColor: "white",
-              "&.Mui-focused": { color: "black", backgroundColor: "white" },
-            },
-          }}
-          variant="filled"
+          required
+          fullWidth
+        />
+        <TextField label="Who is in:" color="secondary" required fullWidth />
+        <TextField
+          label="With:"
+          id="outlined-start-adornment"
+          color="secondary"
           required
           fullWidth
         />
         <TextField
           label="Who is in:"
           id="outlined-start-adornment"
-          sx={{ m: 1, width: "25ch" }}
-          InputProps={{
-            sx: {
-              color: "black",
-              backgroundColor: "white",
-              "&.Mui-focused": { color: "black", backgroundColor: "white" },
-            },
-          }}
-          variant="filled"
+          color="secondary"
           required
           fullWidth
         />
         <TextField
           label="With:"
           id="outlined-start-adornment"
-          sx={{ m: 1, width: "25ch" }}
-          InputProps={{
-            sx: {
-              color: "black",
-              backgroundColor: "white",
-              "&.Mui-focused": { color: "black", backgroundColor: "white" },
-            },
-          }}
-          variant="filled"
+          color="secondary"
           required
           fullWidth
         />
         <TextField
           label="Who is in:"
           id="outlined-start-adornment"
-          sx={{ m: 1, width: "25ch" }}
-          InputProps={{
-            sx: {
-              color: "black",
-              backgroundColor: "white",
-              "&.Mui-focused": { color: "black", backgroundColor: "white" },
-            },
-          }}
-          variant="filled"
+          color="secondary"
           required
           fullWidth
         />
         <TextField
           label="With:"
           id="outlined-start-adornment"
-          sx={{ m: 1, width: "25ch" }}
-          InputProps={{
-            sx: {
-              color: "black",
-              backgroundColor: "white",
-              "&.Mui-focused": { color: "black", backgroundColor: "white" },
-            },
-          }}
-          variant="filled"
+          color="secondary"
           required
           fullWidth
         />
         <TextField
           label="Who is in:"
           id="outlined-start-adornment"
-          sx={{ m: 1, width: "25ch" }}
-          InputProps={{
-            sx: {
-              color: "black",
-              backgroundColor: "white",
-              "&.Mui-focused": { color: "black", backgroundColor: "white" },
-            },
-          }}
-          variant="filled"
+          color="secondary"
           required
           fullWidth
         />
         <TextField
           label="With:"
           id="outlined-start-adornment"
-          sx={{ m: 1, width: "25ch" }}
-          InputProps={{
-            sx: {
-              color: "black",
-              backgroundColor: "white",
-              "&.Mui-focused": { color: "black", backgroundColor: "white" },
-            },
-          }}
-          variant="filled"
+          color="secondary"
           required
           fullWidth
         />
         <TextField
           label="Who is in:"
           id="outlined-start-adornment"
-          sx={{ m: 1, width: "25ch" }}
-          InputProps={{
-            sx: {
-              color: "black",
-              backgroundColor: "white",
-              "&.Mui-focused": { color: "black", backgroundColor: "white" },
-            },
-          }}
-          variant="filled"
+          color="secondary"
           required
           fullWidth
         />
         <TextField
           label="With:"
           id="outlined-start-adornment"
-          sx={{ m: 1, width: "25ch" }}
-          InputProps={{
-            sx: {
-              color: "black",
-              backgroundColor: "white",
-              "&.Mui-focused": { color: "black", backgroundColor: "white" },
-            },
-          }}
-          variant="filled"
-          required
-          fullWidth
-        />
-        <TextField
-          label="Who is in:"
-          id="outlined-start-adornment"
-          sx={{ m: 1, width: "25ch" }}
-          InputProps={{
-            sx: {
-              color: "black",
-              backgroundColor: "white",
-              "&.Mui-focused": { color: "black", backgroundColor: "white" },
-            },
-          }}
-          variant="filled"
-          required
-          fullWidth
-        />
-        <TextField
-          label="With:"
-          id="outlined-start-adornment"
-          sx={{ m: 1, width: "25ch" }}
-          InputProps={{
-            sx: {
-              color: "black",
-              backgroundColor: "white",
-              "&.Mui-focused": { color: "black", backgroundColor: "white" },
-            },
-          }}
-          variant="filled"
+          color="secondary"
           required
           fullWidth
         />
