351a0426c661446e00247455e25d6627df44a867
Ashley Wessel <amerino.dance@gmail.com>
2024-02-20 10:27:50 -0600

    refactored a bit

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,176 +1,3 @@
-// import React, { useEffect } from 'react';
-// import LogOutButton from '../LogOutButton/LogOutButton';
-// import { useHistory } from 'react-router-dom';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { TextField, Button, Grid, Container, Card, CardContent, Paper, Box } from "@mui/material";
-// import Table from '@mui/material/Table';
-// import TableBody from '@mui/material/TableBody';
-// import TableCell from '@mui/material/TableCell';
-// import TableContainer from '@mui/material/TableContainer';
-// import TableHead from '@mui/material/TableHead';
-// import TableRow from '@mui/material/TableRow';
-// import FormControl from '@mui/material/FormControl';
-
-
-// function UserPage() {
-
-//   // added for CreateGame button
-//   const history = useHistory();
-//   const handleClick = () => {
-//     history.push('/new-game');
-//   }
-
-//   const user = useSelector((store) => store.user);
-//   const games = useSelector((store) => store.gameReducer);
-//   const dispatch = useDispatch();
-
-//   useEffect(() => {
-//     dispatch({ type: 'FETCH_GAME', payload: user.id });
-//   }, []);
-
-  
-
-//   return (
-//     <div className="container">
-//       <LogOutButton>log out</LogOutButton>
-//       <h1>Welcome, {user.first_name}!</h1>
-//       <p>Your ID is: {user.id}</p>
-
-    
-
-
-//       <Button
-//         variant='outlined'
-//         onClick={handleClick}
-//         >Create Game</Button>
-      
-
-//       <h2>Now Playing:</h2>
-
-//       {games.length > 0 ?
-//         <div>
-//           {games.map(game => {
-//               if (game.is_ongoing && game.active_respondent_id === user.id) {
-//                 return (
-//                   <>
-//                     {/* <h3>Actor</h3> */}
-//                       <Card key={game.id}>
-//                         <CardContent>
-//                             <h4>game ID: {game.id}</h4>
-//                             <h4>TITLE: Pitt-Jolie</h4>
-//                             <h4>STARRING: {user.first_name} & Friend name based on ID</h4>
-//                             <h4>SCENE: {game.active_scene}</h4>
-//                             <h4>SCORE: shows up when you're on scene 2?</h4>
-//                             <h4>STATUS: You're wanted on set!</h4>
-//                             <Button variant='outlined'>play</Button>                 
-//                         </CardContent>
-//                       </Card>
-//                   </>
-//                 )
-//               } else if (game.is_ongoing && game.active_respondent_id !== user.id) {
-//                   return (
-//                     <>
-//                       {/* <h3>Director</h3> */}
-//                         <Card key={game.id}>
-//                           <CardContent>
-//                               <h4>game ID: {game.id}</h4>
-//                               <h4>TITLE: Pitt-Jolie</h4>
-//                               <h4>STARRING: {user.first_name} & Friend name based on ID</h4>
-//                               <h4>SCENE: {game.active_scene}</h4>
-//                               <h4>SCORE: shows up when you're on scene 2?</h4>
-//                               <h4>STATUS: Waiting for your actor...</h4>
-//                               <Button variant='outlined'>play</Button>                 
-//                           </CardContent>
-//                         </Card>
-//                     </>
-//                   )
-//               } 
-//           })}
-//         </div>
-
-//         :
-
-//         <div>
-//             <h3 id="no-games">No games to display!</h3>
-//         </div>
-//       }
-
-    
-
-      
-
-      
-
-//       {games.length >= 0 ? 
-//         <div>
-//           {games.map(game => {
-//             if (!game.is_ongoing) {
-//               return (
-//                 <>
-//                 <h2>Filmography:</h2>
-//                   <Card key={game.id}>
-//                     <CardContent>
-//                         <h4>game ID: {game.id}</h4>
-//                         <h4>TITLE: Pitt-Jolie</h4>
-//                         <h4>STARRING: {user.first_name} & Friend name based on ID</h4>
-//                         <h4>SCORE: shows up when you're on scene 2?</h4>                                       
-//                     </CardContent>
-//                   </Card>
-//                 </>
-//               )
-//             } 
-//           })}
-//         </div>
-
-//         :
-
-//         <div>
-//           <h2>Filmography:</h2>
-//           <h4>No game history yet!</h4>
-//         </div>
-
-//     }
-  
-//     </div>
-//   );
-// }
-
-// // this allows us to use <App /> in index.js
-// export default UserPage;
-
-
-
-// ^^ this pretty much works but i'm going to refactor a bit
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import React, { useEffect } from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import { useHistory } from 'react-router-dom';
@@ -202,20 +29,38 @@ function UserPage() {
   }, []);
 
   
+  const sendScriptsBack = () => {
+    console.log("in sendScriptsBack");
+  }
+
+  const playScripts = () => {
+    console.log("in playScripts");
+  }
+
+  const viewSentScripts = () => {
+    console.log("in viewSentScripts");
+  }
+
+
 
   return (
     <div className="container">
-      <LogOutButton>log out</LogOutButton>
-      <h1>Welcome, {user.first_name}!</h1>
-      <p>Your ID is: {user.id}</p>
 
-    
+      <LogOutButton>log out</LogOutButton>
+
+      <Grid>
+      <Container sx={{justifyContent: "center"}}>
+        
+      <h1>Welcome, {user.first_name}!</h1>
+      {/* <p>Your ID is: {user.id}</p> */}
+
+      
       <Button
         variant='outlined'
         onClick={handleClick}
         >Create Game</Button>
       
-
+      
       <h2>Now Playing:</h2>
 
       {games.length > 0 ?
@@ -242,7 +87,8 @@ function UserPage() {
                             </div>  
                             }                            
                             <h4>STATUS: You're wanted on set!</h4>
-                            <Button variant='outlined'>play</Button>                 
+                            <Button variant='outlined' onClick={sendScriptsBack}>send scripts back</Button>
+                            <Button variant='outlined' onClick={playScripts}>play</Button>                                             
                         </CardContent>
                       </Card>
                   </>
@@ -272,13 +118,12 @@ function UserPage() {
                             </div>  
                             }  
                             <h4>STATUS: Waiting for your actor...</h4>
-                            <Button variant='outlined'>view sent scripts</Button>                 
+                            <Button variant='outlined' onClick={viewSentScripts}>view sent scripts</Button>                 
                         </CardContent>
                       </Card>
                   </>
                 )
-            } 
-            
+            }             
           })}          
         </div>
 
@@ -334,7 +179,8 @@ function UserPage() {
         }
     {/* ^^ this isn't working quite as expected.... I still want the Filmography: no game history yet! to show up if all the games
     you are in are active */}
-  
+    </Container>
+    </Grid>
     </div>
   );
 }
