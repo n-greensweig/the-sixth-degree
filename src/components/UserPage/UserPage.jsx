6e014852e8561708ab0a359e6e5c788e859ff8f6
Noah Greensweig <ngreensweig@gmail.com>
2024-02-26 14:44:37 -0600

    Pushing new edits.

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -125,7 +125,7 @@ function UserPage() {
                           <CardContent>
                             {
                               Number(game.userScore) === Number(game.playerTwoScore) ? <h4>The game is tied {game.userScore} - {game.playerTwoScore}</h4> :
-                              <h4>You're {game.userScore > game.playerTwoScore ? 'winning' : 'losing'} {game.userScore} - {game.playerTwoScore}</h4>
+                                <h4>You're {Number(game.userScore) > Number(game.playerTwoScore) ? 'winning' : 'losing'} {game.userScore} - {game.playerTwoScore}</h4>
                             }
                             <h4>Starring: You & {game.nonUserFirstName}</h4>
                             {
@@ -171,9 +171,9 @@ function UserPage() {
                       <>
                         <Card key={game.id} className='card'>
                           <CardContent>
-                          {
+                            {
                               Number(game.userScore) === Number(game.playerTwoScore) ? <h4>The game is tied {game.userScore} - {game.playerTwoScore}</h4> :
-                              <h4>You're {game.userScore > game.playerTwoScore ? 'winning' : 'losing'} {game.userScore} - {game.playerTwoScore}</h4>
+                                <h4>You're {Number(game.userScore) > Number(game.playerTwoScore) ? 'winning' : 'losing'} {game.userScore} - {game.playerTwoScore}</h4>
                             }
                             <h4>Starring: You & {game.nonUserFirstName}</h4>
                             {
